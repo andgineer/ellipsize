0f2384d3c1d721158798619e1fd655f8dcb5a3a1
andgineer <Andrei_Sorokin2@epam.com>
2023-11-11 07:53:07 +0100

    unpin dependencies version in the package

--- scripts/verup.sh
+++ scripts/verup.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
-# Increments version git tag
-# Saves it into VERSION_FILE
+# Increments version git tag or create snapshot version from the release current commit was branched from.
+# Saves the version into files listed in VERSION_FILES (space-separated)
+# Usage:
+#   verup.sh release / feature / branch / ss
+# (ss - to create snapshot version)
 
 VERSION_FILES=("src/ellipsize/__about__.py")
 
-TAG=$(git describe --tags)
+set -u  # fail on unset variables
+set -e  # if any command fails, stop execution
+set -x  # print commands
+
+if [[ $(git diff-index HEAD) || $(git status) == *"is ahead"* ]]; then
+  echo -e "\n\033[33mPlease commit and push all changes" \
+    "before setting version tag\033[39m\n"
+  exit -1
+fi
+
+if [[ "$1" == "ss" ]]; then
+  # get the latest non-snapshot backward from current commit
+  TAG=$(git describe --match "v[0-9]*" --abbrev=0 || echo "")
+else
+  # force fetching tags from all branches
+  git fetch --tags
+  # get the latest by creation, from all branches. note! "[0-9]*" does not repeat square brackets -
+  # this is "one digit and any number of any chars".
+  TAG=$(git describe \
+    --match "v[0-9]*.[0-9]*.[0-9]*" \
+    --abbrev=0 \
+    --tags "$(git rev-list --tags --max-count=1)" \
+    || echo "")
+fi
 
 major=0
 minor=0
@@ -19,12 +45,6 @@ fi
 
 echo -e "Last version: \033[33m$major.$minor.$build\033[39m"
 
-if [[ $(git diff-index HEAD) || $(git status) == *"is ahead"* ]]; then
-  echo -e "\n\033[33mPlease commit and push all changes" \
-    "before setting version tag\033[39m\n"
-  exit -1
-fi
-
 if [[ "$1" == "release" ]]; then
   build=0
   minor=0
@@ -34,8 +54,11 @@ elif [[ "$1" == "feature" ]]; then
   minor=$(echo $minor + 1 | bc)
 elif [[ "$1" == "bug" ]]; then
   build=$(echo $build + 1 | bc)
+elif [[ "$1" == "ss" ]]; then
+  major="i$major"
+  build="$build-$(git rev-parse --short HEAD)"
 else
-  echo "usage: ./verup.sh [release|feature|bug]"
+  echo "usage: ./verup.sh [release|feature|bug|ss]"
   exit -1
 fi
 
